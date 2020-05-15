c3575b0cb68f9675ab8d9784707306f4dd2aa515
Ronald Jaglal <ronniejaglal365@gmail.com>
2020-05-15 13:45:59 -0400

    updates on viewing recipes and adding it to list

--- script.js
+++ script.js
@@ -6,27 +6,64 @@ $(document).ready(function(){
   
 });
 
+var proxyUrl ='https://cors-anywhere.herokuapp.com/',
+    url="http://www.recipepuppy.com/api/"
 
 
-const ul= document.getElementById('authors');
-const url = 'https://randomuser.me/api/?results=10';
+async function getData(proxyUrl, url){
+  try{
+    let response = await fetch(proxyUrl + url);
+    let result= await response.json();
+    drawtable(result.results)
+    console.log(result.results)
+  }
+catch(e){
+  console.log(e);
 
+}
+}
 
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-let authors = data.results;
-return authors.map(function(author) {
-  let li = createNode('li'),
-      img = createNode('img'),
-      span = createNode('span');
-  img.src = author.picture.medium;
-  span.innerHTML = `${author.name.first} ${author.name.last}`;
-  append(li, img);
-  append(li, span);
-  append(ul, li);
-})
-})
-.catch(function(error) {
-console.log(error);
-});   
+//code for the button that adds to the list
+// var button = document.createElement('button')
+// button.onclick= function(){
+//   console.log("alert('I was clicked')");
+// }
+// var list
+function Add(){
+  list= list.split(',')
+  while(list[i] != null){
+  ingredients.innerHTML +=`<tr>
+  <div class="items">
+   
+    <li>${list[i]}</li> 
+  </div>
+    `;
+    i=i+1;
+  }
+  console.log(list);
+}
+var list=[];
+var i=0;
+getData(proxyUrl, url)
+
+function drawtable(recipes){
+  let result = document.querySelector('#result');
+  for(let recipe of recipes){
+    list=recipe.ingredients;
+    // console.log(list);
+    result.innerHTML +=`<tr>
+    <div class="items">
+     
+      <h4>${recipe.title}</h4> 
+      <img src=${recipe.thumbnail} width:100px height:100px </img>
+      <br>
+      <a>${recipe.href}</a>
+      <li>${recipe.ingredients}</li>
+      <button type ='button'  onclick ='Add(list)' >ADD</button>
+      
+      </tr>
+      </div>
+      <br>`;
+  }
+}
+
